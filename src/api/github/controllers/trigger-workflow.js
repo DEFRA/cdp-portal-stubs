013cdf9900d25cbbbb27c81d3d6a4b89d966612b
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2024-07-09 13:32:29 +0100

    adds stub for cdp-squid-proxy workflow

--- src/api/github/controllers/trigger-workflow.js
+++ src/api/github/controllers/trigger-workflow.js
@@ -1,4 +1,3 @@
-import { triggerCreateRepoWorkflow } from '~/src/api/github/events/trigger-create-repo-workflow'
 import {
   ecrRepos,
   githubRepos,
@@ -6,55 +5,120 @@ import {
   topicsService,
   topicsTestSuite
 } from '~/src/config/mock-data'
+import { triggerWorkflowStatus } from '~/src/api/github/events/trigger-workflow-status'
 
 const triggerWorkflow = {
   handler: async (request, h) => {
-    const org = request.params.org
-    const repo = request.params.repo
-    const workflowFile = request.params.workflow
-    const inputs = request.payload.inputs
-    const repositoryName = inputs.repositoryName
-
-    request.logger.info(
-      `Stubbing triggering of workflow ${org}/${repo} ${workflowFile} with inputs ${JSON.stringify(
-        inputs
-      )}`
-    )
-
-    await triggerCreateRepoWorkflow(request.sqs, repositoryName)
-
-    if (workflowFile === 'create_microservice.yml') {
-      request.logger.info(`Adding service ${repositoryName} to github repos`)
-      githubRepos.push({ name: repositoryName, topics: topicsService })
-
-      // technically this should happen on the TF-svc stage but its easier to
-      // detect the service type+name here
-      if (ecrRepos[repositoryName] === undefined) {
-        ecrRepos[repositoryName] = { tags: [], runMode: 'service' }
-      }
-    }
-
-    if (workflowFile === 'create_env_test_suite.yml') {
-      request.logger.info(`Adding test suite ${repositoryName} to github repos`)
-      githubRepos.push({ name: repositoryName, topics: topicsTestSuite })
-
-      if (ecrRepos[repositoryName] === undefined) {
-        ecrRepos[repositoryName] = { tags: [], runMode: 'job' }
-      }
-    }
-
-    if (workflowFile === 'create_perf_test_suite.yml') {
-      request.logger.info(
-        `Adding perf test suite ${repositoryName} to github repos`
-      )
-      githubRepos.push({ name: repositoryName, topics: topicsPerfTestSuite })
-
-      if (ecrRepos[repositoryName] === undefined) {
-        ecrRepos[repositoryName] = { tags: [], runMode: 'job' }
-      }
+    const workflowRepo = request.params.repo
+    switch (workflowRepo) {
+      case 'cdp-create-workflows':
+        await handleCdpCreateWorkflows(request)
+        break
+      case 'cdp-squid-proxy':
+        await handleSquidWorkflows(request)
+        break
+      default:
+        return h
+          .response({ message: `unknown workflow ${workflowRepo}` })
+          .code(400)
     }
 
     return h.response({}).code(200)
+  }
+}
+
+const handleCdpCreateWorkflows = async (request) => {
+  const org = request.params.org
+  const workflowFile = request.params.workflow
+  const inputs = request.payload.inputs
+  const repositoryName = inputs.repositoryName
+
+  request.logger.info(
+    `Stubbing triggering of workflow ${org}/cdp-create-workflows/${workflowFile} with inputs ${JSON.stringify(
+      inputs
+    )}`
+  )
+
+  await triggerWorkflowStatus(
+    request.sqs,
+    'cdp-create-workflows',
+    repositoryName,
+    'completed',
+    'success',
+    1
+  )
+  if (workflowFile === 'create_microservice.yml') {
+    request.logger.info(`Adding service ${repositoryName} to github repos`)
+    githubRepos.push({ name: repositoryName, topics: topicsService })
+
+    // technically this should happen on the TF-svc stage but its easier to
+    // detect the service type+name here
+    if (ecrRepos[repositoryName] === undefined) {
+      ecrRepos[repositoryName] = { tags: [], runMode: 'service' }
+    }
+  }
+
+  if (workflowFile === 'create_env_test_suite.yml') {
+    request.logger.info(`Adding test suite ${repositoryName} to github repos`)
+    githubRepos.push({ name: repositoryName, topics: topicsTestSuite })
+
+    if (ecrRepos[repositoryName] === undefined) {
+      ecrRepos[repositoryName] = { tags: [], runMode: 'job' }
+    }
+  }
+
+  if (workflowFile === 'create_perf_test_suite.yml') {
+    request.logger.info(
+      `Adding perf test suite ${repositoryName} to github repos`
+    )
+    githubRepos.push({ name: repositoryName, topics: topicsPerfTestSuite })
+
+    if (ecrRepos[repositoryName] === undefined) {
+      ecrRepos[repositoryName] = { tags: [], runMode: 'job' }
+    }
+  }
+}
+
+const handleSquidWorkflows = async (request) => {
+  const org = request.params.org
+  const workflowFile = request.params.workflow
+  const inputs = request.payload.inputs
+  const service = inputs.service
+
+  request.logger.info(
+    `Stubbing triggering of workflow ${org}/cdp-squid-proxy/${workflowFile} with inputs ${JSON.stringify(
+      inputs
+    )}`
+  )
+
+  if (workflowFile === 'create_service.yml') {
+    request.logger.info(`Adding service ${service} to squid config`)
+    await triggerWorkflowStatus(
+      request.sqs,
+      'cdp-squid-proxy',
+      service,
+      'requested',
+      null,
+      0
+    )
+
+    await triggerWorkflowStatus(
+      request.sqs,
+      'cdp-squid-proxy',
+      service,
+      'in_progress',
+      null,
+      1
+    )
+
+    await triggerWorkflowStatus(
+      request.sqs,
+      'cdp-squid-proxy',
+      service,
+      'completed',
+      'success',
+      2
+    )
   }
 }
 
