f07606f1a214475be33bd7f2fccc1666958f5e2d
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-02-26 09:47:38 +0000

    adds slack channels to team stub

--- src/config/teams-and-users.js
+++ src/config/teams-and-users.js
@@ -1,6 +1,6 @@
 /**
  *
- * @type { {teams: {team_id: string, name: string, description: string, github: string|null, service_code: string|null}[], users: *[] }}
+ * @type {{ teams: {team_id: string, name: string, description: string, github: string|null, service_code: string|null, slack_channels: { prod: string, non_prod: string, team: string }}[], users: *[] }}
  */
 const teamsAndUsers = {
   teams: [
@@ -8,13 +8,23 @@ const teamsAndUsers = {
       team_id: 'platform',
       name: 'Platform',
       description: 'The team that runs the platform',
-      github: 'cdp-platform'
+      github: 'cdp-platform',
+      slack_channels: {
+        team: 'cdp-platform-team',
+        prod: 'cdp-platform-alerts',
+        non_prod: 'cdp-platform-non-prod-alerts'
+      }
     },
     {
       team_id: 'tenantteam1',
       name: 'TenantTeam1',
       description: 'A test team',
-      github: 'cdp-tenant-1'
+      github: 'cdp-tenant-1',
+      slack_channels: {
+        team: 'tenantteam1',
+        prod: 'tenantteam1-prod-alerts',
+        non_prod: 'tenantteam1-non-prod-alerts'
+      }
     }
   ],
   users: []
