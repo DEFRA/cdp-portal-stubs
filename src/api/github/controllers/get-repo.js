e765c3c6ed048732d73e5d7d5f0fff6ea0912a22
Chris Turner <23338096+christopherjturner@users.noreply.github.com>
2026-09-21 14:00:24 +0100

    CORE-4009 improves get repo stub

--- src/api/github/controllers/get-repo.js
+++ src/api/github/controllers/get-repo.js
@@ -1,12 +1,24 @@
 import { generateRepoData } from '~/src/api/github/content/repo-data'
+import { githubRepos } from '~/src/config/mock-data'
 
 const getRepoController = {
   handler: async (request, h) => {
     const { org, repo } = request.params
+
+    if (githubRepos.some((githubRepo) => repo === githubRepo.name)) {
+      return h
+        .response(generateRepoData(org, repo))
+        .header('x-oauth-scopes', 'TODO')
+        .code(200)
+    }
+
     return h
-      .response(generateRepoData(org, repo))
-      .header('x-oauth-scopes', 'TODO')
-      .code(200)
+      .response({
+        message: 'Not Found',
+        documentation_url: 'https://docs.github.com/rest',
+        status: '404'
+      })
+      .code(404)
   }
 }
 
